8de02b05eea5fb9fc65e8c5813dca6a6056c79d6
SammysHP <sven@sammyshp.de>
2013-09-19 16:54:00 +0200

    Refactoring: Make compatible with Java 6
    Necessary change in theme file: translate language 3-letter-codes into 2-letter-codes

--- src/main/java/de/freizeitkarteosm/themeconfigurator/parser/Theme.java
+++ src/main/java/de/freizeitkarteosm/themeconfigurator/parser/Theme.java
@@ -84,7 +84,7 @@ class Theme {
             final Map<Locale, String> names = new HashMap<Locale, String>();
             for (String t : translations) {
                 final String[] tt = t.split("===");
-                names.put(Locale.forLanguageTag(tt[0]), tt[1]);
+                names.put(new Locale(tt[0]), tt[1]);
             }
 
             int startLine = -1;
